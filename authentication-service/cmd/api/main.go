6a496382d017c55a05ea52f17f8b86b6cda7e1a5
auliardana <auliarifki45@gmail.com>
2024-03-06 08:56:29 +0700

    first commit

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"authentication/data"
+)
+
+
+const webPort = "80"
+
+type Config struct {
+	DB *sql.DB
+	Models data.Models
+}
+func main(){
+	log.Println("Starting authentication service on port", webPort)
+
+	//todo connect to DB
+
+
+
+
+
+
+
+}
+
+
+
---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error: false,
+		Message: "Hit the broker",
+		Data: nil,
+	}
+
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
+}
